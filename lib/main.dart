6236f93c40b3288f10bb0ac32020e2f2f5917c2a
rashsvr <rashmi.s.v.rajapaksha@gmail.com>
2025-01-25 19:01:37 +0530

    initial change in to admin login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:saloon_app/firebase_options.dart';
+import 'package:saloon_app/screens/admin/login/admin_login_view.dart';
 import 'package:saloon_app/screens/admin/registration/admin_signup_view.dart';
 import 'package:saloon_app/screens/splash/splash_1/splash_1_view.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Beauty Qatar',
       debugShowCheckedModeBanner: false,
-      home: AdminSignUpView(),
+      home: AdminLoginView(),
      
     );
   }
