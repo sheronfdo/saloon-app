2547bdb6f0b0252ef7e198263a1ea55d33c62610
sheronfdo <jamithsheron5@gmail.com>
2025-01-26 18:48:58 +0530

    edit package service

--- lib/services/admin/flow/service/addNewPackage/add_new_package.dart
+++ lib/services/admin/flow/service/addNewPackage/add_new_package.dart
@@ -24,4 +24,25 @@ class AddNewPackageService {
       'status': 'active'
     });
   }
+
+  Future<void> editPackage(
+      {required String catId,
+        required String packageId,
+        required String title,
+        required double price,
+        required String description}) async {
+    await _firestore
+        .collection('admins')
+        .doc(_auth.currentUser?.uid)
+        .collection("categories")
+        .doc(catId)
+        .collection("packages")
+        .doc(packageId)
+        .set({
+      'title': title,
+      'price': price,
+      'description': description,
+      'status': 'active'
+    });
+  }
 }
